3aad054df918b0dd5eaa5e3f2f5aadebac355a8d
TarasRogiv <r.taras.t.1@gmail.com>
2024-12-24 12:35:31 +0200

    1 tasc done

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 const firstResButton = document.getElementById("first-task-res-button");
-const quantityField = document.getElementById("quantity");
-const pricePerDroidField = document.getElementById("pricePerDroid");
-const customerCredits = document.getElementById("customerCreditsCount");
+const messageField = document.getElementById("messageInputField");
 const resField = document.getElementById("first-task-result-field");
 
 firstResButton.addEventListener("click", () => {
-  resField.value = makeTransaction(
-    quantityField.value,
-    pricePerDroidField.value,
-    customerCredits.value
-  );
+  resField.value = slugify(messageField.value);
 });
 
 // Друге завдання
